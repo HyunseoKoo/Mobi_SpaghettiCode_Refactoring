e7569d9d8af0a01a8086ff9a738e54c2c34e8e9c
hyunseoKoo <koohs414@naver.com>
2023-07-26 12:09:26 +0900

    create: 스파게티 코드 생성

---
+++ src/third-party/weather.config.js
@@ -0,0 +1,6 @@
+export const weatherConfig = (() => {
+  return {
+    api: "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0",
+    secret_key: "+559LYZFeQCbWx9jaekI3DW1o11grdaFK3ZyTG//Kki372fdRN16DBNLIIstr+rCqQxDps8/0leZxkkr143Wag==",
+  };
+})();
